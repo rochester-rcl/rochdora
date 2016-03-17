653134b5663bfcf1fe02ee1d2753fa300c672fca
nate-rcl <nsarr@library.rochester.edu>
2016-03-17 13:04:22 -0400

    updated to switch based on occulded setting

--- js/book_page_compound_block.js
+++ js/book_page_compound_block.js
@@ -12,18 +12,40 @@
             var old_page_update = Drupal.settings.islandora_paged_tei_seadragon_update_page;
             Drupal.settings.islandora_paged_tei_seadragon_update_page = function (pid, page_number) {
                 // Drop out here if we are the most current request.
-                if (pid == Drupal.settings.islandora_paged_tei_seadragon.current_page) {
+                if (pid === Drupal.settings.islandora_paged_tei_seadragon.current_page) {
                     return;
                 }
                 old_page_update(pid, page_number);
 
+                // get the url parameters
+                (function () {
+                    var match,
+                            pl = /\+/g, // Regex for replacing addition symbol with a space
+                            search = /([^&=]+)=?([^&]*)/g,
+                            decode = function (s) {
+                                return decodeURIComponent(s.replace(pl, " "));
+                            },
+                            query = window.location.search.substring(1);
+
+                    urlParams = {};
+                    while (match = search.exec(query))
+                        urlParams[decode(match[1])] = decode(match[2]);
+                })();
+                var myUrl = Drupal.settings.basePath + "rochdora/compound_block/" + pid;
+                
+                if (urlParams && urlParams['occluded'] && urlParams['occluded'] === "true") {
+                    myUrl = Drupal.settings.basePath + "rochdora/compound_block/" + pid + "?occluded=true";;
+                }
+
                 // Update compound block.
                 $.ajax({
-                    url: Drupal.settings.basePath + "rochdora/compound_block/" + pid,
+                    url:myUrl,
                     cache: false,
                     success: function(response) {
+                        
+                        
                         // Drop out here if we are not the most current request.
-                        if (pid != Drupal.settings.islandora_paged_tei_seadragon.current_page) {
+                        if (pid !== Drupal.settings.islandora_paged_tei_seadragon.current_page) {
                             return;
                         }
                         // Kill the block.
